8d7446697670d97dfe0ccb42d2e80174a30152f4
edhernandez04 <mchh.004@gmail.com>
2020-02-28 17:08:23 -0500

    MAJOR MOVES DUDE

--- backend_rails/app/controllers/vehicles_controller.rb
+++ backend_rails/app/controllers/vehicles_controller.rb
@@ -1,12 +1,8 @@
 class VehiclesController < ApplicationController
 
-
-
     def index
         vehicles = Vehicle.all
-
         render json: vehicles
     end
-
 
 end
--- backend_rails/config/routes.rb
+++ backend_rails/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-get '/index', to: 'vehicles#index'
+get '/vehicles', to: 'vehicles#index'
 
 end
